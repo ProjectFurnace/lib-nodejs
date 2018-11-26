2afba2181b58bc3a6c41d6415d7184ae5113438c
Danny Waite <dannywaite7@gmail.com>
2018-11-26 20:35:47 +0000

    added full type defs for fsutils

--- packages/fsutils/index.d.ts
+++ packages/fsutils/index.d.ts
@@ -1,1 +1,9 @@
 export function exists(file: string): boolean;
+export function mkdir(dir: string): void;
+export function rimraf(file: string): void;
+export function createTempDirectory(): string;
+export function createTempPath(): string;
+export function cp(src: string, dest: string): void;
+export function writeFile(file: string, contents: string): void;
+export function listDirectory(dir: string): string[];
+export function stat(file: string): any;
