97035c02c600964cb91e7bb1b8c1a67e96233dae
Danny Waite <dannywaite7@gmail.com>
2020-04-22 10:21:52 +0100

    options optional in cp

--- packages/fsutils/index.d.ts
+++ packages/fsutils/index.d.ts
@@ -3,7 +3,7 @@ export function mkdir(dir: string): void;
 export function rimraf(file: string): void;
 export function createTempDirectory(): string;
 export function createTempPath(): string;
-export function cp(src: string, dest: string, options: object): void;
+export function cp(src: string, dest: string, options?: object): void;
 export function writeFile(file: string, contents: string): void;
 export function listDirectory(dir: string): string[];
 export function stat(file: string): any;
