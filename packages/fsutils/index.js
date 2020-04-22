cf224f9f190b40fd468de711e51dd30e959012d4
Danny Waite <dannywaite7@gmail.com>
2020-04-22 10:15:37 +0100

    pass options to cp

--- packages/fsutils/index.js
+++ packages/fsutils/index.js
@@ -1,47 +1,45 @@
-const fs = require("fs")
-    , rimraf = require("rimraf")
-    , fsExtra = require("fs-extra")
-    , tmp = require("tmp")
-    ;
-
+const fs = require("fs"),
+  rimraf = require("rimraf"),
+  fsExtra = require("fs-extra"),
+  tmp = require("tmp");
 module.exports.exists = (file) => {
-    return fs.existsSync(file);
-}
+  return fs.existsSync(file);
+};
 
 module.exports.mkdir = (dir) => {
-    fs.mkdirSync(dir);
-}
+  fs.mkdirSync(dir);
+};
 
 module.exports.rimraf = (file) => {
-    rimraf.sync(file);
-}
+  rimraf.sync(file);
+};
 
 module.exports.createTempDirectory = () => {
-    const dir = tmp.dirSync().name;
-    return dir;
-}
+  const dir = tmp.dirSync().name;
+  return dir;
+};
 
 module.exports.createTempPath = () => {
-    return tmp.fileSync().name;
-}
+  return tmp.fileSync().name;
+};
 
-module.exports.cp = (src, dest) => {
-    fsExtra.copySync(src, dest);
-}
+module.exports.cp = (src, dest, options) => {
+  fsExtra.copySync(src, dest, options);
+};
 
 module.exports.writeFile = (file, contents) => {
-    fs.writeFileSync(file, contents);
-}
+  fs.writeFileSync(file, contents);
+};
 
 module.exports.listDirectory = (dir) => {
-    const files = fs.readdirSync(dir);
-    return files;
-}
+  const files = fs.readdirSync(dir);
+  return files;
+};
 
 module.exports.stat = (file) => {
-    return fs.lstatSync(file);
-}
+  return fs.lstatSync(file);
+};
 
 module.exports.readFile = (file) => {
-    return fs.readFileSync(file, "utf8");
-}
+  return fs.readFileSync(file, "utf8");
+};
